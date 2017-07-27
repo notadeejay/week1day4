0ee52302780465d24f8c7110322e493d591c362b
Amy <amy@lighthouselabs.ca>
2017-07-27 20:35:03 +0000

    Returns a new number from an array each time

--- loadeddie.js
+++ loadeddie.js
@@ -1,15 +1,18 @@
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  var randomElement = list[Math.floor(Math.random()*list.length)]
+   var i = -1;
 
-
-  return function() {
-   return randomElement;
+   return function() {
+    i++
+    return list[i];
   }
-}
+};
+
+
+
 
 var rollLoadedDie = makeLoadedDie();
+console.log(rollLoadedDie()); // 5
 
-console.log(rollLoadedDie());  // 5
 
 
